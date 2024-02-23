9166b457ff09b9eb9f314e0d7d4b1df6c99a2508
hugodrw <hugoderohanwillner@gmail.com>
2024-02-23 17:31:48 +0100

    trying to fix delta_u

--- InPlanePhysics.py
+++ InPlanePhysics.py
@@ -1,4 +1,5 @@
 from astropy import units as u
+import numpy as np
 
 def delta_u(r1, r2):
     # Assuming r1 and r2 are in units of length (e.g., meters)
@@ -7,5 +8,5 @@ def delta_u(r1, r2):
     
     # Calculate the angle
     angle = u.rad * (1 - (1 - (r1 + r2) / (2 * r2))**0.5)
-    
+    # angle = u.rad * ((1 - ((r1 + r2) / (2 * r2))**0.5)*np.pi)*2
     return angle << u.deg
