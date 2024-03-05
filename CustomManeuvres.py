59638b29d94520ce6d36bd11fcba85c995de0b40
hugodrw <hugoderohanwillner@gmail.com>
2024-03-05 15:59:44 +0100

    working!

--- CustomManeuvres.py
+++ CustomManeuvres.py
@@ -43,6 +43,7 @@ def time_to_inc_change(orbit):
 def time_to_raan_change(orbit_i, orbit_f):
     '''
         Compute inc change thrust location and time of flight to reach raan change location
+        Must be done AFTER inc change
     '''
     # Compute required values for equations
     delta_raan = (orbit_f.raan - orbit_i.raan) << u.rad
@@ -61,7 +62,7 @@ def time_to_raan_change(orbit_i, orbit_f):
         delta_u = 360 * u.deg + delta_u # wrap to 360
     time = (orbit_i.period << u.s) * delta_u / (360 * u.deg)
     
-    return time, u_final
+    return time, u_final, theta
     
 def hohmann_with_phasing(orbit_i: Orbit, orbit_f: Orbit, debug=True):
     r"""Compute a Hohmann transfer with correct phasing to a target debris.
@@ -197,50 +198,43 @@ def simple_raan_change(orbit_i: Orbit, orbit_f: Orbit, debug=True):
     ----------
 
     """
-    # Compute thrust location
-    time_to_thrust, thrust_location = time_to_raan_change(orbit_i, orbit_f)
+    # Compute thrust location and theta
+    time_to_thrust, thrust_location, theta = time_to_raan_change(orbit_i, orbit_f)
     orbit_i = orbit_i.propagate(time_to_thrust)
 
-    return time_to_thrust, thrust_location
+    # Calculate the thrust value
+    v = norm(orbit_i.v << u.m / u.s)
+    inc_i = orbit_i.inc << u.rad
+    inc_f = orbit_f.inc << u.rad
+    inc_delta = -theta
+    thrust_norm = 2*v*np.sin((inc_delta << u.rad)/2)
 
-    # # Calculate the thrust value
-    # v = norm(orbit_i.v << u.m / u.s)
-    # inc_i = orbit_i.inc << u.rad
-    # inc_f = orbit_f.inc << u.rad
-    # inc_delta = inc_f - inc_i
-    # thrust_norm = 2*v*np.sin((inc_delta << u.rad)/2)
-
-    # # Calculate the thrust vector
-    # y_thrust = np.sin(inc_delta/2)*thrust_norm
-    # z_thrust = -np.cos(inc_delta/2)*thrust_norm
+    # Calculate the thrust vector
+    y_thrust = np.sin(inc_delta/2)*thrust_norm
+    z_thrust = -np.cos(inc_delta/2)*thrust_norm
     
-    # if debug:
-    #     print('thrust_norm', thrust_norm)
-    #     print('y_thrust', y_thrust)
-    #     print('z_thrust', z_thrust)
+    if debug:
+        print('thrust_norm', thrust_norm)
+        print('y_thrust', y_thrust)
+        print('z_thrust', z_thrust)
 
-    # # Rotate values through nu
-    # # thrust_vector = np.array([0 ,y_thrust.value,z_thrust.value]) * u.m / u.s
-    # nu = orbit_i.nu << u.rad
-    # print('nu: ', nu )
-    # print(-np.sin(nu))
-    # print(-np.cos(nu))
-    # thrust_vector = np.array([-np.sin(nu)*y_thrust.value ,np.cos(nu)*y_thrust.value,z_thrust.value]) * u.m / u.s
+    # Rotate values through nu
+    # thrust_vector = np.array([0 ,y_thrust.value,z_thrust.value]) * u.m / u.s
+    nu = orbit_i.nu << u.rad
+    print('nu: ', nu )
+    print(-np.sin(nu))
+    print(-np.cos(nu))
+    thrust_vector = np.array([-np.sin(nu)*y_thrust.value ,np.cos(nu)*y_thrust.value,z_thrust.value]) * u.m / u.s
+    thrust_vector = - thrust_vector
 
-    # if thrust_location == 0 * u.deg:
-    #     print('zero deg transformation')
-    #     thrust_vector = - thrust_vector
-    # else:
-    #     thrust_vector[1] = - thrust_vector[1]
+    # Use rotation matrix to go from orbital to general referential
+    k = orbit_i.attractor.k
+    rv = orbit_i.rv()
+    rv = (rv[0].to_value(u.m), rv[-1].to_value(u.m / u.s))
+    _, ecc, inc, raan, argp, nu = rv2coe(k, *rv)
+    rot_matrix = coe_rotation_matrix(inc, raan, argp)
+    thrust_vector = rot_matrix @ thrust_vector
 
-    # # Use rotation matrix to go from orbital to general referential
-    # k = orbit_i.attractor.k
-    # rv = orbit_i.rv()
-    # rv = (rv[0].to_value(u.m), rv[-1].to_value(u.m / u.s))
-    # _, ecc, inc, raan, argp, nu = rv2coe(k, *rv)
-    # rot_matrix = coe_rotation_matrix(inc, raan, argp)
-    # thrust_vector = rot_matrix @ thrust_vector
-
-    # return Maneuver(
-    #     (time_to_thrust.decompose(), thrust_vector.decompose())
-    # )
+    return Maneuver(
+        (time_to_thrust.decompose(), thrust_vector.decompose())
+    )
